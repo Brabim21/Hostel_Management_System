ecfce5830f6ea3df8a1a050c768c104322ed7b20
Rojan Dhungel <120009180+rojan-dhungel@users.noreply.github.com>
2024-04-14 18:25:10 +0545

    Add files via upload

---
+++ staff/home.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hostel Mangement System</title>
+</head>
+<body>
+    This is a home page for staff.
+</body>
+</html>
