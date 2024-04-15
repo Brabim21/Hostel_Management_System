dcb08f79d00a3faaf641a8a6d63dfd8100a148fc
Brabim21 <119724645+Brabim21@users.noreply.github.com>
2024-04-15 10:21:35 +0545

    Update home.php
    UI for staff dashboard is updated with the features required.

--- staff/home.php
+++ staff/home.php
@@ -3,9 +3,37 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hostel Mangement System</title>
+    <title>Staff Dashboard</title>
+    <link rel="stylesheet" href="staffDashboard.css">
 </head>
 <body>
-    This is a home page for staff.
+    <header>
+        <h1>Staff Dashboard</h1>
+        <p id="account">Account</p>
+    </header>
+    <main>
+        <nav id="sidebar">
+            <h2>Dashboard</h2>
+            <ul>
+                <li><a href="#">Dashboard</a></li>
+                <li><a href="#">Resident Details</a></li>
+                <li><a href="#">Event Calendar</a></li>
+
+            </ul>
+        </nav>
+        <section class="content">
+            <h2>Dashboard</h2>
+            <div class="card" id="total-residents">
+                <h3>Total Residents: 100</h3>
+            </div>
+            <div class="card" id="upcoming-events">
+                <h3>Upcoming Events</h3>
+                <p>Event 1</p>
+                <p>Event 2</p>
+                <p>Event 3</p>
+            </div>
+        </section>
+    </main>
+    <script src="script.js"></script>
 </body>
 </html>
