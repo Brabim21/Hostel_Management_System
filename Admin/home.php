72c02c874574307018ce10a0aec019bd8b2df8eb
Ganeshparajuli2059 <119146652+Ganeshparajuli2059@users.noreply.github.com>
2024-04-22 09:33:36 +0545

    Update home.php
    updated new backend for admin

--- Admin/home.php
+++ Admin/home.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hostel Management System - Admin Page</title>
-    <link rel="stylesheet" href=" AdminDashboard.css">
+    <link rel="stylesheet" href="AdminDashboard.css">
 </head>
 <body>
     <nav>
