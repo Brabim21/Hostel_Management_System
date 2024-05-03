b25a69a2bd3956f8939216605703edf8085286a4
Rojan Dhungel <120009180+rojan-dhungel@users.noreply.github.com>
2024-05-03 21:51:56 +0545

    Updated home.php

--- staff/home.php
+++ staff/home.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hostel Management System - Staff Page</title>
     <link rel="stylesheet" href="style.css">
-    <script src="script.js" defer></script> <!-- Include your JavaScript file -->
 </head>
 <body>
     <nav>
