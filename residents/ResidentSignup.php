0f4e77f64927a136c97e085a50aa448ed9493006
Brabim21 <119724645+Brabim21@users.noreply.github.com>
2024-05-23 23:54:54 +0545

    Update ResidentSignup.php

--- residents/ResidentSignup.php
+++ residents/ResidentSignup.php
@@ -9,42 +9,51 @@
     <img src="Hostel.jpg" alt="Hostel Image" class="background-image">
     <h1>Hostel Management System</h1>
     <h2>Sign Up</h2>
-    <form id="signup-form" action="signup_process.php" method="post" enctype="multipart/form-data">
-      <label for="name">Name:</label>
-      <input type="text" id="name" name="name" placeholder="Your name">
-      <?php if (isset($_GET['name_err'])) echo "<p class='error'>" . $_GET['name_err'] . "</p>"; ?>
+    <div class="login-container">
+      <form id="signup-form" action="signup_process.php" method="post" enctype="multipart/form-data">
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name" placeholder="Your name">
+        <?php if (isset($_GET['name_err'])) echo "<p class='error'>" . $_GET['name_err'] . "</p>"; ?>
 
-      <label for="email">Email:</label>
-      <input type="text" id="email" name="email" placeholder="Your email">
-      <?php if (isset($_GET['email_err'])) echo "<p class='error'>" . $_GET['email_err'] . "</p>"; ?>
+        <label for="email">Email:</label>
+        <input type="text" id="email" name="email" placeholder="Your email">
+        <?php if (isset($_GET['email_err'])) echo "<p class='error'>" . $_GET['email_err'] . "</p>"; ?>
 
-      <label for="password">Password:</label>
-      <input type="password" id="password" name="password" placeholder="Your password">
-      <?php if (isset($_GET['password_err'])) echo "<p class='error'>" . $_GET['password_err'] . "</p>"; ?>
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" placeholder="Your password">
+        <?php if (isset($_GET['password_err'])) echo "<p class='error'>" . $_GET['password_err'] . "</p>"; ?>
 
+      <div class="citizenship-container">
       <label for="citizenship_front">Citizenship Front:</label>
-      <input type="file" id="citizenship_front" name="citizenship_front">
+        <input type="file" id="citizenship_front" name="citizenship_front">
 
-      <label for="citizenship_back">Citizenship Back:</label>
-      <input type="file" id="citizenship_back" name="citizenship_back">
+        <label for="citizenship_back">Citizenship Back:</label>
+        <input type="file" id="citizenship_back" name="citizenship_back">
+      </div>
+  
+      <input type="submit" name="signup" value="Sign Up" class="button orange-button">
 
-      <input type="submit" name="signup" value="Sign Up">
+      
     </form>
-    <div class="error-messages">
-      <?php
-      // Display error messages if they exist
-      if (isset($_GET['name_err'])) {
-          echo "<p class='error'>" . $_GET['name_err'] . "</p>";
-      }
-      if (isset($_GET['email_err'])) {
-          echo "<p class='error'>" . $_GET['email_err'] . "</p>";
-      }
-      if (isset($_GET['password_err'])) {
-          echo "<p class='error'>" . $_GET['password_err'] . "</p>";
-      }
-      ?>
+
+        <div class="error-messages">
+              <?php
+              // Display error messages if they exist
+              if (isset($_GET['name_err'])) {
+                  echo "<p class='error'>" . $_GET['name_err'] . "</p>";
+              }
+              if (isset($_GET['email_err'])) {
+                  echo "<p class='error'>" . $_GET['email_err'] . "</p>";
+              }
+              if (isset($_GET['password_err'])) {
+                  echo "<p class='error'>" . $_GET['password_err'] . "</p>";
+              }
+              ?>
+            </div>
+            <a href="ResidentLogin.php" class="button orange-button" id="existing-user">Existing User</a>
+          </div>
     </div>
-    <a href="ResidentLogin.php">Existing User</a>
-  </div>
 </body>
+    
 </html>
+
